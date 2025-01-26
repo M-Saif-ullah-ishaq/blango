f83929173e50559e53a3165d672277f6a616d2d4
Muhammad ishaq <muhammadsaifullahishaq1@gmail.com>
2025-01-26 10:31:28 +0000

    Finish custom filters

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+import bloog.views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url('',bloog.views.index),
 ]
--- blog/bloog/views.py
+++ blog/bloog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from bloog.models import Post
 
 # Create your views here.
+def index(request):
+    posts = Post.objects.filter(published_at__lte=timezone.now())
+    return render(request, "bloog/templates/index.html", {"posts": posts})
